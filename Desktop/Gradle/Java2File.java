8e3ae01dac4ee26881ba37a5a48e6b14bf2cdc2a
SharaRizvi <47344683+SharaRizvi@users.noreply.github.com>
2019-03-07 16:52:21 +0530

    Add files via upload

---
+++ Desktop/Gradle/Java2File.java
@@ -0,0 +1,11 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+public class Java2File {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle from Java2 folder!!");
+    }
+}
---
+++ Desktop/Gradle/JavaSourceFile.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class JavaSourceFile{
+    public static void main(String[] args) {
+        String str="Shara";
+        System.out.println(StringUtils.reverse(str));
+    }
+}
